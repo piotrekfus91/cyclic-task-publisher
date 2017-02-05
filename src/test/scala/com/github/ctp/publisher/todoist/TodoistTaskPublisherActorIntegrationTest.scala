69f1e1f062d83b5982e57ec4012a32ce77db828f
piotrekfus91 <piotrek.fus.91@gmail.com>
2017-02-05 20:37:18 +0100

    Travis CI integration tests

--- src/test/scala/com/github/ctp/publisher/todoist/TodoistTaskPublisherActorIntegrationTest.scala
+++ src/test/scala/com/github/ctp/publisher/todoist/TodoistTaskPublisherActorIntegrationTest.scala
@@ -1,17 +1,25 @@
 package com.github.ctp.publisher.todoist
 
 import java.util.UUID
+import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
 import com.github.ctp.config.domain.{Task, TodoistUser, UserData}
 import com.github.ctp.guice._
-import com.github.ctp.publisher.{Publish, PublisherSequence}
 import com.github.ctp.publisher.todoist.service.ProjectListManager
+import com.github.ctp.publisher.{Publish, PublisherSequence}
 import com.github.ctp.test.TodoistIntegrationTestContext._
 import com.google.inject.Guice
 import org.scalatest._
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Millis, Seconds, Span}
 
-class TodoistTaskPublisherActorIntegrationTest extends FlatSpec with Matchers with BeforeAndAfterEach {
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+class TodoistTaskPublisherActorIntegrationTest extends FlatSpec with Matchers with BeforeAndAfterEach
+    with BeforeAndAfterAll with Eventually {
+
   val guice = Guice.createInjector(
     new AkkaModule,
     new LoggerModule,
@@ -41,13 +49,18 @@ class TodoistTaskPublisherActorIntegrationTest extends FlatSpec with Matchers wi
   it should "create task in real Todoist" taggedAs TodoistMustBeAvailable in {
     todoistTaskPublisher ! Publish(userData, task, PublisherSequence(List()))
 
-    actorSystem.stop(todoistTaskPublisher)
-
-    getAllTasks should include(taskName)
+    eventually(timeout(Span(2, Seconds)), interval(Span(100, Millis))) {
+      getAllTasks should include(taskName)
+    }
   }
 
   override protected def afterEach(): Unit = {
     deleteTestProject()
+  }
+
+  override protected def afterAll(): Unit = {
+    actorSystem.terminate()
+    Await.ready(actorSystem.whenTerminated, Duration(1, TimeUnit.SECONDS))
   }
 }
 
